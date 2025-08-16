0e91505d6cfebcd3d2c325d5ddd6fcfba1ad5d79
Filip Półtoraczyk <filip.poltoraczyk@bcf-software.com>
2025-08-16 07:26:58 +0200

    Wip

---
+++ app/controllers/mqtt_handlers/conn_handler.py
@@ -0,0 +1,58 @@
+import logging
+from app.core.db_context import async_session_maker
+from app.core.mqtt.base_mqtt_callback_handler import BaseMqttCallbackHandler
+from app.models.dtos.notifications import NotificationCreateDTO
+from app.repos.esp_devices import EspDeviceRepository
+from app.repos.notifications import NotificationRepository
+from app.repos.users import UserRepository
+from app.services.notifications import NotificationService
+from app.services.readings import ReadingService
+from app.repos.readings import ReadingRepository
+from app.models.enums import DeviceType, NotificationType
+from app.models.dtos.readings import ReadingCreateDTO
+
+logger = logging.getLogger(__name__)
+
+SENSOR_STR_TO_DEVICE_TYPE = {
+    "light": DeviceType.LIGHT_SENSOR,
+    "air_humidity": DeviceType.AIR_HUMIDITY_SENSOR,
+    "soil_moisture": DeviceType.SOIL_MOISTURE_SENSOR,
+    "air_temperature": DeviceType.AIR_TEMPERATURE_SENSOR,
+    "signal_strenght": DeviceType.SIGNAL_STRENGHT,
+    "battery": DeviceType.BATTERY
+}
+
+
+class DeviceReadingHandler(BaseMqttCallbackHandler):
+    def __init__(self):
+        super().__init__("{mac}/conn")
+
+    async def __call__(self, topic: str, payload: dict):
+        logger.info(f"[CONN] topic={topic}, payload={payload}")
+
+        user_key = payload.get("userKey")
+        if user_key is None:
+            logger.warning(
+                f"Missing 'user_key' in {payload}.")
+            return
+
+        try:
+            mac = self.extract_from_topic(topic, "mac")
+        except (ValueError, TypeError):
+            logger.warning(f"Could not extract valid mac from topic: {topic}")
+            return
+
+        async with async_session_maker() as session:
+            esp_repo = EspDeviceRepository(session)
+            esp = await esp_repo.get_by_mac(mac)
+
+            user = await UserRepository(session).get_by_user_key(user_key)
+            if not user:
+                logger.warning(f"No user found for key {user_key}")
+                return
+
+            await esp_repo.update(esp.id, user_id=user.id)
+
+            not_service = NotificationService(NotificationRepository(session))
+            not_service.create(NotificationCreateDTO(
+                user_id=user.id, message=f"ESP {esp.mac} is attached to your account", type=NotificationType.alert))
---
+++ app/controllers/mqtt_handlers/device_reading_handler.py
@@ -0,0 +1,58 @@
+import logging
+from app.controllers.mqtt_handlers.base_device_handler import BaseDeviceHandler
+from app.core.db_context import async_session_maker
+from app.services.readings import ReadingService
+from app.repos.readings import ReadingRepository
+from app.models.enums import DeviceType
+from app.models.dtos.readings import ReadingCreateDTO
+
+logger = logging.getLogger(__name__)
+
+SENSOR_STR_TO_DEVICE_TYPE = {
+    "light": DeviceType.LIGHT_SENSOR,
+    "air_humidity": DeviceType.AIR_HUMIDITY_SENSOR,
+    "soil_moisture": DeviceType.SOIL_MOISTURE_SENSOR,
+    "air_temperature": DeviceType.AIR_TEMPERATURE_SENSOR,
+    "signal_strenght": DeviceType.SIGNAL_STRENGHT,
+    "battery": DeviceType.BATTERY
+}
+
+
+class DeviceReadingHandler(BaseDeviceHandler):
+    def __init__(self):
+        super().__init__("{mac}/device/sensor")
+
+    async def __call__(self, topic: str, payload: dict):
+        logger.info(f"[SENSOR] topic={topic}, payload={payload}")
+
+        sensor_str = payload.get("sensor")
+        if sensor_str not in SENSOR_STR_TO_DEVICE_TYPE:
+            logger.warning(f"Unknown sensor type '{sensor_str}' in payload.")
+            return
+        device_type = SENSOR_STR_TO_DEVICE_TYPE[sensor_str]
+
+        value = payload.get("value")
+        if value is None:
+            logger.warning(f"Missing 'value' in {device_type.name} payload.")
+            return
+
+        try:
+            mac = self.extract_from_topic(topic, "mac")
+        except (ValueError, TypeError):
+            logger.warning(f"Could not extract valid mac from topic: {topic}")
+            return
+
+        device, _ = await self.process_device_event(
+            topic,
+            mac,
+            device_type,
+            payload,
+            websocket_event="new_reading",
+            extra_fields={"value": value},
+        )
+
+        if device:
+            async with async_session_maker() as session:
+                reading_service = ReadingService(ReadingRepository(session))
+                dto = ReadingCreateDTO(device_id=device.id, value=str(value))
+                await reading_service.create(dto)
